16a1a953de8fd20909ec8d06382a91be85768e4c
Adityasharma0709 <7rockyrocky@gmail.com>
2024-08-13 20:33:54 +0530

    yupp!!

--- src/components/productList.js
+++ src/components/productList.js
@@ -1,17 +1,84 @@
+// // // ProductList.js
+
+// import React, { useState } from 'react';
+// import { Link } from 'react-router-dom';
+// import { data } from '../assests/itemData'; 
+// import '../css/productList.css'; 
+// import PaginationButtons1 from '../components/PaginationButton';
+
+// const itemsPerPage = 20;
+
+// const ProductList = () => {
+//   const [currentPage, setCurrentPage] = useState(1);
+
+//   const handlePageChange = (newPage) => {
+//     setCurrentPage(newPage);
+//   };
+
+//   const startIndex = (currentPage - 1) * itemsPerPage;
+//   const visibleItems = data.slice(startIndex, startIndex + itemsPerPage);
+
+//   return (
+//     <>
+//       <div className="product-list">
+//         {visibleItems.map((item) => (
+//           <Link to={`/product/${item.id}`} key={item.id} className="product-item">
+//             <img src={require(`../assests/${item.image}`)} alt={item.name} />
+//             <p>{item.name}</p>
+//           </Link>
+//         ))}
+//       </div>
+//       <PaginationButtons1
+//         totalItems={data.length}
+//         itemsPerPage={itemsPerPage}
+//         onPageChange={handlePageChange}
+//       />
+//     </>
+//   );
+// };
+
+// export default ProductList;
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
-import React from 'react';
-import {data} from '../assests/itemData'; // Adjust the path accordingly
-import '../css/productList.css'; // Add your styling
+import { data } from '../assests/itemData';
+import '../css/productList.css';
+import PaginationButtons1 from '../components/PaginationButton';
+
+const itemsPerPage = 20;
+
 const ProductList = () => {
+  const dispatch = useDispatch();
+  const products = useSelector((state) => state.product.products);
+  const currentPage = useSelector((state) => state.product.currentPage);
+
+  useEffect(() => {
+    dispatch({ type: 'SET_PRODUCTS', payload: data });
+  }, [dispatch]);
+
+  const handlePageChange = (newPage) => {
+    dispatch({ type: 'SET_CURRENT_PAGE', payload: newPage });
+  };
+
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const visibleItems = products.slice(startIndex, startIndex + itemsPerPage);
+
   return (
-    <div className="product-list">
-      {data.map(item => (
-        <Link to={`/product/${item.id}`} key={item.id} className="product-item" >
-          <img src={require(`../assests/${item.image}`)} alt={item.name}/>
-          <p >{item.name}</p>
-        </Link>
-      ))}
-    </div>
+    <>
+      <div className="product-list">
+        {visibleItems.map((item) => (
+          <Link to={`/product/${item.id}`} key={item.id} className="product-item">
+            <img src={require(`../assests/${item.image}`)} alt={item.name} />
+            <p>{item.name}</p>
+          </Link>
+        ))}
+      </div>
+      <PaginationButtons1
+        totalItems={products.length}
+        itemsPerPage={itemsPerPage}
+        onPageChange={handlePageChange}
+      />
+    </>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-
+import { Provider } from "react-redux";
+import store from './store/store'
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+   <Provider store={store}>
     <App />
+  </Provider>
   </React.StrictMode>
 );
 
